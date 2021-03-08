ea13d9d8e1af6a7ba8d6654389fb9994d39ee49a
Mandy Wade <lightninggirl413@hotmail.com>
2021-03-08 15:35:36 -0500

    Static HTML for Home Page

--- ClientApp/src/App.jsx
+++ ClientApp/src/App.jsx
@@ -3,6 +3,26 @@ import './custom.scss'
 
 export function App() {
   return (
-    <div className="d-flex justify-content-center display-2">Hello, World!</div>
+    <div>
+      <header>
+        <ul>
+          <li>Welcome, Francie!</li>
+          <li>Login</li>
+          <li>SignUp</li>
+        </ul>
+        <h1>Hidden Mickeys</h1>
+      </header>
+      <main>
+        <button>Browse All</button>
+        <button>Add</button>
+        <button>Scavenger Hunt</button>
+      </main>
+      <footer>
+        <p>
+          &copy; This Hidden Mickey App was created with &hearts; by Mandy Wade
+          2021
+        </p>
+      </footer>
+    </div>
   )
 }
