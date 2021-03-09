3c2b9cc0130189190244e2c2cee57018ebfb1215
Mandy Wade <lightninggirl413@hotmail.com>
2021-03-09 13:29:39 -0500

    Added sample data

---
+++ Models/exampledata.sql
@@ -0,0 +1,6 @@
+TRUNCATE TABLE "HiddenMickeys";
+
+INSERT INTO "HiddenMickeys" ("Clue", "Hint") VALUES ('In the attraction’s 3-D film itself, there is a series of Hidden Mickeys, everybody look left in The Lion King scene. (There are 3 possible options)', 'In The Lion King sequence, look to the far left of the screen; there is a grouping of trees that form a classic Hidden Mickey.');
+INSERT INTO "HiddenMickeys" ("Clue", "Hint") VALUES ('A classic Hidden Mickey can be found on the short rock wall to the left of the restaurant’s check-in desk.', 'It’s on top of the last flat rectangular stone before the wall ends at the left side of the check-in station.');
+INSERT INTO "HiddenMickeys" ("Clue", "Hint") VALUES ('Being the avid reader that Belle is, it would only seem appropriate there are extra special bookends in the window of this shop.', 'The bookends in the outside window form the shape of Mickey’s head.');
+INSERT INTO "HiddenMickeys" ("Clue", "Hint") VALUES ('On the footbridge extending from Pinocchio Village Haus to the Enchanted Forest, there are horse hoof prints embedded in the cement walkways.', 'Find three hoof prints strategically situated together to form a classic Hidden Mickey.');
