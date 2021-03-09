2b804878038e5810bce880e507745d4afc0f0610
Mandy Wade <lightninggirl413@hotmail.com>
2021-03-09 11:33:47 -0500

    Added static HTML and CSS for the update HM page and moved it into its oen file

--- ClientApp/src/App.jsx
+++ ClientApp/src/App.jsx
@@ -4,7 +4,8 @@ import { HiddenMickey } from './pages/HiddenMickey'
 import { HiddenMickeys } from './pages/HiddenMickeys'
 import { Home } from './pages/Home'
 import { ScavengerHunt } from './pages/ScavengerHunt'
+import { UpdateHiddenMickeyPage } from './UpdateHiddenMickeyPage'
 
 export function App() {
-  return <ScavengerHunt />
+  return <UpdateHiddenMickeyPage />
 }
---
+++ ClientApp/src/UpdateHiddenMickeyPage.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+
+export function UpdateHiddenMickeyPage() {
+  return (
+    <>
+      <header>
+        <h2>Hidden Mickey #ID</h2>
+      </header>
+      <p className="disclaimer">Thank you for updating our error!</p>
+      <ul className="hidden-mickey">
+        <li>Park: Magic Kingdom</li>
+        <li>Area: Main Street USA</li>
+        <form>
+          <li>
+            <label htmlFor="Location">Location: </label>
+            <input
+              type="text"
+              placeholder="Railroad Station"
+              value=""
+              name="Location"
+              onChange=""
+            ></input>
+          </li>
+          <li>
+            <label htmlFor="Clue">Clue: </label>
+            <textarea rows="10" cols="30" name="Clue">
+              The train station above the Park’s entry area has a Hidden Mickey
+              situated along the top portion of the train station itself.
+            </textarea>
+          </li>
+          <li>
+            <label htmlFor="Hint">Hint: </label>
+            <textarea rows="10" cols="30" name="Hint">
+              The wrought iron gate work has a repeating Hidden Mickey head
+              design running along the roof.
+            </textarea>
+          </li>
+        </form>
+      </ul>
+    </>
+  )
+}
