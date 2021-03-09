7b384a163cc00d2cab30e3f63b7972682988d8c5
Mandy Wade <lightninggirl413@hotmail.com>
2021-03-08 19:14:16 -0500

    Moved the Specific Hidden Mickey page to it's own file

---
+++ ClientApp/src/HiddenMickey.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+export function HiddenMickey() {
+  return (
+    <>
+      <header>
+        <h2>Hidden Mickey #ID</h2>
+      </header>
+      <ul>
+        <li>Park: Magic Kingdom</li>
+        <li>Area: Main Street USA</li>
+        <li>Location: Railroad Station</li>
+        <li>
+          Clue: The train station above the Park’s entry area has a Hidden
+          Mickey situated along the top portion of the train station itself.
+        </li>
+        <li>
+          Hint: The wrought iron gate work has a repeating Hidden Mickey head
+          design running along the roof.
+        </li>
+      </ul>
+      <article>
+        <button>Back</button>
+        <button>Delete</button>
+        <button>Home</button>
+        <button>Update</button>
+      </article>
+    </>
+  )
+}
