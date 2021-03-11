64d91924265e390c8284dc271e0495850e4ed91a
Mandy Wade <lightninggirl413@hotmail.com>
2021-03-11 14:10:26 -0500

    Added data to the parks table in the example data

--- Models/exampledata.sql
+++ Models/exampledata.sql
@@ -4,3 +4,8 @@ INSERT INTO "HiddenMickeys" ("Location", "Clue", "Hint") VALUES ('Mickey’s Phi
 INSERT INTO "HiddenMickeys" ("Location", "Clue", "Hint") VALUES ('Be Our Guest Restaurant', 'A classic Hidden Mickey can be found on the short rock wall to the left of the restaurant’s check-in desk.', 'It’s on top of the last flat rectangular stone before the wall ends at the left side of the check-in station.');
 INSERT INTO "HiddenMickeys" ("Location", "Clue", "Hint") VALUES ('Bon Jour! Village Gifts', 'Being the avid reader that Belle is, it would only seem appropriate there are extra special bookends in the window of this shop.', 'The bookends in the outside window form the shape of Mickey’s head.');
 INSERT INTO "HiddenMickeys" ("Location", "Clue", "Hint") VALUES ('The Enchanted Forest Grounds', 'On the footbridge extending from Pinocchio Village Haus to the Enchanted Forest, there are horse hoof prints embedded in the cement walkways.', 'Find three hoof prints strategically situated together to form a classic Hidden Mickey.');
+
+INSERT INTO "Parks" ("Name") VALUES ('Magic Kingdom');
+INSERT INTO "Parks" ("Name") VALUES ('Epcot');
+INSERT INTO "Parks" ("Name") VALUES ('Hollywood Studios');
+INSERT INTO "Parks" ("Name") VALUES ('Animal Kingdom');
