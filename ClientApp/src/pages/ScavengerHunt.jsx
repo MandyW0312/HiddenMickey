d75ea6b1bfbf9440c24bdefc2233a0815344a2b7
Mandy Wade <lightninggirl413@hotmail.com>
2021-03-20 20:04:07 -0400

    Made this into the specific scavenger hunt page

--- ClientApp/src/pages/ScavengerHunt.jsx
+++ ClientApp/src/pages/ScavengerHunt.jsx
@@ -1,90 +1,32 @@
 import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router'
 import { Link } from 'react-router-dom'
 
-export function Icon({ name, style = 'fas' }) {
-  return <i className={`${style} fa-${name}`}></i>
-}
-
 export function ScavengerHunt() {
-  const [parks, setParks] = useState([])
   const [mickeys, setMickeys] = useState([])
 
-  const [selectedPark, setSelectedPark] = useState({
-    id: undefined,
-    name: '',
-    areaOfTheParks: [],
-  })
-
-  const [parkDropdownShown, setParkDropdownShown] = useState(false)
-
-  useEffect(function () {
-    async function fetchParks() {
-      const response = await fetch(`/api/Parks`)
-      const json = await response.json()
-      setParks(json)
-    }
-    fetchParks()
-  }, [])
+  const params = useParams()
 
   useEffect(
     function () {
-      if (selectedPark.id === undefined) {
-        return
-      }
-      async function fetchMickeys() {
-        const response = await fetch(
-          `/api/ScavengerHunts?parkId=${selectedPark.id}`,
-          {
-            method: 'POST',
-            headers: { 'content-type': 'application/json' },
-          }
-        )
+      async function fetchMickey() {
+        // @ts-ignore
+        const response = await fetch(`/api/ScavengerHunts/${params.id}`)
         const json = await response.json()
+
         setMickeys(json.scavengerHuntMickeys)
       }
-      fetchMickeys()
+      fetchMickey()
     },
-    [selectedPark.id]
+    // @ts-ignore
+    [params.id]
   )
-
-  console.log(mickeys)
 
   return (
     <>
       <header className="hunt-header">
-        <h2>Scavenger Hunt</h2>
+        <h2>Scavenger Hunt # </h2>
       </header>
-      <article className="shDropdown">
-        <div className="dropdown">
-          <button
-            className="dropdown-parksb-hunt"
-            onClick={function () {
-              setParkDropdownShown(!parkDropdownShown)
-            }}
-          >
-            Park Name
-          </button>
-          <div
-            className={`dropdown-parksc-hunt ${
-              parkDropdownShown ? 'show' : ''
-            }`}
-          >
-            {parks.map(function (park) {
-              return (
-                <button
-                  key={park.id}
-                  onClick={function (event) {
-                    setSelectedPark(park)
-                    setParkDropdownShown(false)
-                  }}
-                >
-                  {park.name}
-                </button>
-              )
-            })}
-          </div>
-        </div>
-      </article>
       <p className="disclaimer">
         Please Note: We recommend that if a Hidden Mickey is during a ride to
         read the more specific hint before riding.
